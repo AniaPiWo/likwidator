3348c9f297a74f06fa7e02609473c0d59cb54263
AniaPiWo <snowflake@wp.pl>
2024-09-13 13:14:24 +0200

    page tsx

---
+++ src/app/form-building/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BuildingForm } from "@/components/BuildingForm";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <BuildingForm />;
+};
+
+export default page;
